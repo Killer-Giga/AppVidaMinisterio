c8e5169e0d0653cfac44a25392d4fc55fa2041cc
Dan Duran <d117j@hotmail.com>
2025-02-28 13:06:55 -0600

    docs(json): cambio en los comentarios

--- Services/DataStorageService.cs
+++ Services/DataStorageService.cs
@@ -10,8 +10,6 @@ namespace AppVidaMinisterio.Services
         /*
          Recibe la Dictionary<int, Semana> y la agrega a un json.
          Estas semanas se acumulan en el mismo archivo y se van agregando mas al final de este.
-         Además esta clase debe contar con la manera de editar el json según la _semanaActual en los entry y editor que hay por semana para actualizar 
-        los nombres en las asignaciones. Esto se debe hacer automáticamente cuando haya algún cambio. 
 
         Debe contar con un metodo para borrar la semanas anteriores a la actual. Y un metodo para actualizar las semanas siguentes que no esten 
         ya guardadas en el archivo. Por ejemplo si tengo los 2 meses proximos ya guardados no debe actualizarlos de nuevo porque podria borrar las semanas ya editadas.
