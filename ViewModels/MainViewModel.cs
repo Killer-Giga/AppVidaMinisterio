d1e06987158e39a506a4b4825ce81d0df712d08e
Dan Duran <d117j@hotmail.com>
2025-03-05 13:57:05 -0600

    Feat(animacion): Se agregan propiedades para ejecutar animaciones de carga

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -5,7 +5,7 @@ using System.Windows.Input;
 
 namespace AppVidaMinisterio.ViewModels
 {
-    internal class MainViewModel : INotifyPropertyChanged
+    public class MainViewModel : INotifyPropertyChanged
     {
         // Propiedades para el Data Binding
         public event PropertyChangedEventHandler? PropertyChanged;
@@ -14,16 +14,16 @@ namespace AppVidaMinisterio.ViewModels
         public ICommand SiguienteComand { get; }
         public ICommand AnteriorComand { get; }
         public ICommand GuardarComand { get; }
-        public ICommand GeneratePdf { get; }
-        public ICommand NewWeeks { get; }
-        public ICommand DeleteWeeks { get; }
+        public ICommand GeneratePdfComand { get; }
+        public ICommand NewWeeksComand { get; }
+        public ICommand DeleteWeeksComand { get; }
+        public ICommand LoadDataCommand { get; }
 
         // Variables y propiedades para las semanas
         DataStorageService dataStorageService = new DataStorageService();
         SortedDictionary<int, Semana> weeks = new SortedDictionary<int, Semana>();
         // Semana del año que se muestra en la pantalla de inicio. Esta conformado por año y numero de semana
         private int _weekNumber = 0;
-
         private Semana? _semanaActual;
         public Semana? SemanaActual
         {
@@ -45,32 +45,55 @@ namespace AppVidaMinisterio.ViewModels
         public bool IsVisibleAsignacionMejoresMaestros4 => !string.IsNullOrEmpty(SemanaActual?.MejoresMaestros.Asignacion4);
         public bool IsVisibleAsignacionVidaCristiana2 => !string.IsNullOrEmpty(SemanaActual?.VidaCristiana.Asignacion2);
 
+        // Propiedades para animaciones
+        private bool _isLoading;
+        public bool IsLoading
+        {
+            get => _isLoading;
+            set
+            {
+                _isLoading = value;
+                OnPropertyChanged();
+            }
+        }
+        public bool IsNotLoading => !IsLoading;
+
         // Constructores 
         public MainViewModel()
         {
             SiguienteComand = new Command(NextWeek);
             AnteriorComand = new Command(PreviousWeek);
             GuardarComand = new Command(SaveJson);
-            GeneratePdf = new Command(PdfGenerator);
-            NewWeeks = new Command(GetNewWeeks);
-            DeleteWeeks = new Command(DeleteOldWeeks);
+            GeneratePdfComand = new Command(PdfGenerator);
+            NewWeeksComand = new Command(async () => await GetNewWeeksAsync());
+            DeleteWeeksComand = new Command(DeleteOldWeeks);
+            // Es necesario? Tal vez solo para pruebas
+            LoadDataCommand = new Command(async () => await LoadDataAsync());
         }
 
         public async Task InitializeAsync()
         {
             // Al inciar la app se verifica si ya se han descargado las semanas. Si es asi se leen del archivo json.
-            var getWeeksService = new GetWeeksService();
-            _weekNumber = getWeeksService.GetCurrentDate();
-            if (!File.Exists(dataStorageService.PathStorage))
+            try
             {
-                weeks = await getWeeksService.GetWeeks();
-                SaveJson();
-                SemanaActual = weeks[_weekNumber];
+                IsLoading = true;
+                var getWeeksService = new GetWeeksService();
+                _weekNumber = getWeeksService.GetCurrentDate();
+                if (!File.Exists(dataStorageService.PathStorage))
+                {
+                    weeks = await getWeeksService.GetWeeks();
+                    SaveJson();
+                    SemanaActual = weeks[_weekNumber];
+                }
+                else
+                {
+                    weeks = await dataStorageService.ReadJsonAsync();
+                    SemanaActual = weeks[_weekNumber];
+                }
             }
-            else
+            finally
             {
-                weeks = await dataStorageService.ReadJsonAsync();
-                SemanaActual = weeks[_weekNumber];
+                IsLoading = false;
             }
         }
 
@@ -117,18 +140,26 @@ namespace AppVidaMinisterio.ViewModels
         }
 
         private void PdfGenerator()
-        { 
+        {
             if (SemanaActual != null)
             {
                 PdfGeneratorService pdfGeneratorService = new PdfGeneratorService(SemanaActual);
             }
         }
 
-        private void GetNewWeeks()
+        private async Task GetNewWeeksAsync()
         {
-            var getWeekService = new GetWeeksService();
-            _ = getWeekService.GetNewWeeks(weeks);
-            SaveJson();
+            try
+            {
+                IsLoading = true;
+                var getWeekService = new GetWeeksService();
+                await getWeekService.GetNewWeeks(weeks);
+                SaveJson();
+            }
+            finally
+            {
+                IsLoading = false;
+            }
         }
 
         private void DeleteOldWeeks()
@@ -136,17 +167,30 @@ namespace AppVidaMinisterio.ViewModels
             // Metodo para borrar las semanas anteriores a la actual
             var getWeekService = new GetWeeksService();
             int currentWeek = getWeekService.GetCurrentDate();
+            List<int> keys = new List<int>();
 
             foreach (var key in weeks)
             {
-                int currentkey = key.Key;
-                if (currentkey < currentWeek)
-                    weeks.Remove(currentkey);
+                if (key.Key < currentWeek)
+                    keys.Add(key.Key);
+            }
+
+            foreach (int key in keys)
+            {
+                weeks.Remove(key);
             }
         }
 
+        private async Task LoadDataAsync()
+        {
+            // Es necesario? NO ES NECESARIO ES SOLO PARA PRUEBAS
+            IsLoading = true;
+            await Task.Delay(3000);
+            IsLoading = false;
+        }
+
         // Metodos para el Data Binding
-        protected virtual void OnPropertyChanged(string propertyName)
+        protected virtual void OnPropertyChanged(string? propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
