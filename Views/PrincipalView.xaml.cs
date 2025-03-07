3995cd591f1a7da92fb070486d170a3a3f4fb59f
Dan Duran <d117j@hotmail.com>
2025-03-06 18:28:28 -0600

    Feat(View): Animacion al dar click en siguiente y atras.

--- Views/PrincipalView.xaml.cs
+++ Views/PrincipalView.xaml.cs
@@ -1,20 +1,87 @@
 using AppVidaMinisterio.ViewModels;
+using AppVidaMinisterio.Messages;
+using CommunityToolkit.Mvvm.Messaging;
 
 namespace AppVidaMinisterio.Views;
 
 public partial class PrincipalView : ContentPage
 {
-	public PrincipalView()
-	{
-        _ = InitializeAsync();
+    public PrincipalView(MainViewModel viewModel)
+    {
+        InitializeComponent();
+        BindingContext = viewModel;
+        InitializeAsync(viewModel);
     }
 
-	private async Task InitializeAsync()
-	{
-        var viewModel = new MainViewModel();
+    private async void InitializeAsync(MainViewModel viewModel)
+    {
         await viewModel.InitializeAsync();
-        BindingContext = viewModel;
+    }
 
-        InitializeComponent();
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+        WeakReferenceMessenger.Default.Register<AnimationMessage>(this, async (r, m) =>
+        {
+            if (m.AnimationType == "ButtonNext")
+            {
+                await AnimateButton(ButtonNext);
+                await AnimateGrid();
+                
+            }
+            else if (m.AnimationType == "ButtonPrevious")
+            {
+                await AnimateButton(ButtonPrevious);
+                await AnimateGrid();
+            }
+        });
+    }
+
+    protected override void OnDisappearing()
+    {
+        WeakReferenceMessenger.Default.Unregister<AnimationMessage>(this);
+        base.OnDisappearing();
+    }
+
+    private async Task AnimateButton(VisualElement element)
+    {
+        await element.ScaleTo(0.9, 100);
+        await element.ScaleTo(1, 100);
+    }
+
+    private async Task AnimateGrid()
+    {
+        var fadeOutTasks = MyGrid.Children
+        .Select(child =>
+        {
+            if (child != borderSkip && child is VisualElement ve)
+                return ve.FadeTo(0, 200);
+            else if (child == borderSkip)
+                return labelSemana.FadeTo(0, 200);
+            else
+                return Task.CompletedTask;
+        })
+        .ToList();
+
+        await Task.WhenAll(fadeOutTasks);
+
+        if (BindingContext is MainViewModel vm)
+        {
+            vm.UpdateWeekAfterAnimation(); // Método que actualiza SemanaActual
+        }
+
+        var fadeInTasks = MyGrid.Children
+        .Select(child =>
+        {
+            if (child != borderSkip && child is VisualElement ve)
+                return ve.FadeTo(1, 200);
+            else if (child == borderSkip)
+                return labelSemana.FadeTo(1, 200);
+            else
+                return Task.CompletedTask;
+        })
+        .ToList();
+
+        await Task.WhenAll(fadeInTasks);
     }
 }
